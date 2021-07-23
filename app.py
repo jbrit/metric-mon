dbf1f3d5267d75b5aa832366df4bc496a33fc731
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-07-23 14:43:29 +0100

    last value cache added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
-from metric_scraper import get_families
+from metric_scraper import fetch_families, get_families
 
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+
+fetch_families()
 
 @app.route("/")
 def hello_world():
--- metric_scraper.py
+++ metric_scraper.py
@@ -1,11 +1,19 @@
+from datetime import datetime
 import requests
 from prometheus_client.parser import text_string_to_metric_families
 
+last_fetched = datetime.now()
+families = []
 
-def get_families():
-    metrics = requests.get('http://18.206.191.172:9100/metrics').text
+def fetch_families():
+    response = requests.get('http://18.206.191.172:9100/metrics')
+    global families
+    global last_fetched
+    if not response:
+        return families
+    last_fetched = datetime.now()
+    metrics = response.text
     families = []
-
     for family in text_string_to_metric_families(metrics):
         samples = []
         for sample in family.samples:
@@ -22,3 +30,9 @@ def get_families():
             "samples": samples,
         })
     return families
+
+def get_families():
+    """
+    Returns last cached values
+    """
+    return {"last_fetched": str(last_fetched), "families": families}
